0d7734082366de8d47f29d07d94830896d2b9449
zlmind <laynemind@gmail.com>
2021-05-29 18:30:28 +0800

    Create nav.config.ts
    dd

--- nav.config.ts
+++ nav.config.ts
@@ -26,7 +26,7 @@ const c: IConfig = {
   keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
 
   // 默认主题: Light | Sim | Side | App | Shortcut
-  theme: 'Light',
+  theme: 'sim',
 
   // 网站底部内容, 版权信息、备案号, 可以是 HTML
   footerContent: `
